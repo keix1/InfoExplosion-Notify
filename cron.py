56695ce0bd10a2b6d5ffb3b9efffcf7e596cf481
ke_ix1 <xkei4dev@gmail.com>
2018-05-10 07:58:50 +0900

    modify docker can exec

--- cron.py
+++ cron.py
@@ -10,29 +10,32 @@ import time
 
 def main():
     while True:
-        notifier = Notifier()
-        dc = database.DatabaseController()
+        try:
+            notifier = Notifier()
+            dc = database.DatabaseController()
 
-        tweets = twitterprovider.get_tweets()
-        keywords = []
-        for row in dc.get_all_keyword():
-            keywords.append(row[1])
+            tweets = twitterprovider.get_tweets()
+            keywords = []
+            for row in dc.get_all_keyword():
+                keywords.append(row[1])
 
-        tweet = random.choice(tweets)['text']
-        keyword = random.choice(keywords)
+            tweet = random.choice(tweets)['text']
+            keyword = random.choice(keywords)
 
-        print(tweet, keyword)
+            print(tweet, keyword)
 
-        gs = GoogleSearch()
-        search_word = tweet + " " + keyword
-        gs.search(search_word)
-        gs.view()
-        result_title = random.choice(gs.get_articles())[0]
-        result_url = random.choice(gs.get_articles())[1]
-        print(result_title)
+            gs = GoogleSearch()
+            search_word = tweet + " " + keyword
+            gs.search(search_word)
+            gs.view()
+            result_title = random.choice(gs.get_articles())[0]
+            result_url = random.choice(gs.get_articles())[1]
+            print(result_title)
 
-        notifier.notify(title=search_word, body=result_title, data=result_url)
-        time.sleep(60 * 20)
+            notifier.notify(title=search_word, body=result_title, data=result_url)
+            time.sleep(60 * 20)
+        except Exception as e:
+            pass
 
 
 if __name__=='__main__':

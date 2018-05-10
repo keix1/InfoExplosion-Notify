97335c50586add9e94aad47d1742f8a45cae5e74
ke_ix1 <xkei4dev@gmail.com>
2018-05-10 23:40:50 +0900

    現状はcron.pyを動かすため、main.pyの中身を削除

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ from time import sleep
 from news import newsprovider
 
 def main():
-    notifier = Notifier()
-    news_p = newsprovider.News_Provider()
-
-    while(True):
-        for info in news_p.get_news():
-            title = "News"
-            body = info['text']
-            data = "データ"
-            notifier.notify(title, body, data)
-            print(title + "," + body + "," + data)
-            sleep(1)
-
-        sleep(20)
-
+    pass
 if __name__ == '__main__':
     main()
